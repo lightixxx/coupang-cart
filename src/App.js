bf3bda010c1966e3ce199f08ab17c550a3ca6397
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-05-16 22:09:53 +0900

    🎉 Basic set-up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import data from "../data.js"
+
+console.log(data)
+const products = data.products
+export default function App() {
+  return (
+      <div>
+        hello
+      </div>
+  )
+}
