dde96bb91f30e4065640ba6644cc92db83521bf2
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-05-17 17:34:45 +0900

    💄 Add a table and image data binding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
 import data from "../data.js"
+import RocketFreshIcon from "./components/RocketIcon/RocketFreshIcon.js";
+import RocketWowIcon from "./components/RocketIcon/RocketWowIcon.js";
 
 console.log(data)
 const products = data.products
+function type({type}) {
+  switch (type) {
+    case "ROCKET_FRESH" : return <RocketFreshIcon />;
+    case "ROCKET_WOW" : return <RocketWowIcon />;
+    default: return ""
+  }
+}
 export default function App() {
   return (
       <div>
-        hello
+        <table>
+          <tbody>
+            {products.map(product => {
+              return (
+                <tr key={product.id}>
+                  <td>
+                    <input type={"checkbox"} />
+                  </td>
+                  <td>
+                    <img src={product.img} alt={product.name} />
+                  </td>
+                  <td>
+                    {product.name}
+                  </td>
+                  <td>
+                    {product.price}
+                  </td>
+                  <td>
+                    <select>
+                      <option>1</option>
+                    </select>
+                  </td>
+                  <td>
+                    {product.price * product.count}
+                  </td>
+                  <td>
+                    {type(product)}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
   )
 }
